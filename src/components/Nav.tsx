520b5934ea512bf0f89cb0233cc142eab85ff431
DEE <donzydonathan@gmail.com>
2023-06-27 16:20:13 +0100

    feat: add all images to public dir

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+import avatar from "/avatar.png";
+
 export default function Nav() {
+  const [toggled, setToggled] = useState(false);
   return (
     <nav>
       <h1>Logo</h1>
